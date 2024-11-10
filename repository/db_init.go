9934658d99c79591861e2ba720a71b426a12bb70
Greyson <392644099@qq.com>
2024-11-10 15:55:04 +0800

    “修正postIndexMap的初始化方式”

--- repository/db_init.go
+++ repository/db_init.go
@@ -59,6 +59,7 @@ func initPostIndexMap(filePath string) error {
 			PostTmpMap[post.ParentId] = []*Post{&post}
 			continue
 		}
+		posts = append(posts, &post)
 		PostTmpMap[post.ParentId] = posts
 	}
 	postIndexMap = PostTmpMap
