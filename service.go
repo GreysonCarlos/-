f4f4f69929205e24dda7042abe416e5957374c50
Greyson <392644099@qq.com>
2024-11-10 15:50:18 +0800

    "更改了代码结构，方便dlv调试"

--- service.go
+++ service.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"github.com/GreysonCarlos/Topic-web/repository"
-	"github.com/GreysonCarlos/Topic-web/controller"
-	"github.com/gin-gonic/gin"
+	"log"
 	"os"
+	"github.com/GreysonCarlos/Topic-web/controller"
+	"github.com/GreysonCarlos/Topic-web/repository"
+	"github.com/gin-gonic/gin"
 )
 
+func Handler(c *gin.Context) {
+	topicId := c.Param("id")
+	data := controller.QueryPageInfo(topicId)
+	c.JSON(200, data)
+}
 func main() {
-	if err := Init("../data/"); err != nil {
+	if err := Init("./data/"); err != nil {
 		os.Exit(-1)
 	}
 	r := gin.Default()
-	r.GET("/community/page/get/:id", func(c *gin.Context) {
-			topicId := c.Param("Id")
-			data := controller.QueryPageInfo(topicId)
-			c.JSON(200, data)
-	})
+	r.GET("/community/page/get/:id", Handler)
 	err := r.Run()
 	if err != nil {
+		log.Printf("Run failed: %#v", err)
 		return
 	}
 }
 
 func Init(filePath string) error {
 	if err := repository.Init(filePath); err != nil {
+		log.Printf("Filepath error: %#v", err)
 		return err
 	}
 	return nil
 }
+// "/home/greyson/ginweb/data/post"
