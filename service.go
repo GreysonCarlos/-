2a20eec8b3488c4f10173cd8350ac2163fac2ce2
Greyson <392644099@qq.com>
2024-11-10 18:16:42 +0800

    “同步post逻辑”

--- service.go
+++ service.go
@@ -13,12 +13,21 @@ func Handler(c *gin.Context) {
 	data := controller.QueryPageInfo(topicId)
 	c.JSON(200, data)
 }
+
+func Postfunc(c *gin.Context) {
+	topicId, _ := c.GetPostForm("topic_id")
+	content, _ := c.GetPostForm("content")
+	data := controller.PublishPost(topicId, content)
+	c.JSON(200, data)
+}
 func main() {
 	if err := Init("./data/"); err != nil {
 		os.Exit(-1)
 	}
 	r := gin.Default()
 	r.GET("/community/page/get/:id", Handler)
+	
+	r.POST("/community/post/do", Postfunc)
 	err := r.Run()
 	if err != nil {
 		log.Printf("Run failed: %#v", err)
@@ -33,4 +42,3 @@ func Init(filePath string) error {
 	}
 	return nil
 }
-// "/home/greyson/ginweb/data/post"
